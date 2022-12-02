011fd8b2b5c9db13ae2295881e22d85d64c45b40
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-12-02 12:05:13 -0500

    feat: use `ConfigService` to load app port

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -28,8 +29,9 @@ async function bootstrap() {
   // -**********************************-
 
   const logger = new Logger('Main');
-  const PORT = 3000;
-  await app.listen(process.env.PORT || PORT, '0.0.0.0', () => {
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('app.port');
+  await app.listen(PORT, '0.0.0.0', () => {
     logger.log(`API running on port: ${PORT}`);
   });
 }
